48464436568971710016b973f821044a4e36f488
zakafir <zak.afir@gmail.com>
2017-08-06 03:33:24 +0200

    AsyncTask successfully enabled

--- app/src/main/java/com/st00/afir/footballapp/PlayersActivity.java
+++ app/src/main/java/com/st00/afir/footballapp/PlayersActivity.java
@@ -1,24 +1,55 @@
 package com.st00.afir.footballapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 import java.util.List;
 
 public class PlayersActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = Utils.class.getSimpleName();
+
+    /**
+     * URL for players data
+     */
+    private static final String FOOTBALL_REQUEST_URL = "http://api.football-data.org/v1/teams/87/players";
+
+    private ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players);
 
-        QueryUtils queryUtils = new QueryUtils();
-        List<Player> list = queryUtils.extractPlayers();
+        PlayersAsyncTask playersAsyncTask = new PlayersAsyncTask();
+        playersAsyncTask.execute(FOOTBALL_REQUEST_URL);
 
-        ListView myListView = (ListView) findViewById(R.id.list_view_players);
-        PlayersAdapter<Player> adapter = new PlayersAdapter<>(this, list);
-        myListView.setAdapter(adapter);
+        myListView = (ListView) findViewById(R.id.list_view_players);
+
+    }
+
+    private class PlayersAsyncTask extends AsyncTask<String, Void, List<Player>> {
+
+        @Override
+        protected List<Player> doInBackground(String... strings) {
+            if (strings.length < 1 || strings[0] == null) {
+                return null;
+            }
+            List<Player> list = Utils.fetchPlayersData(strings[0]);
+            return list;
+        }
+
+        @Override
+        protected void onPostExecute(List<Player> list) {
+            if (list == null) {
+                return;
+            }
+            PlayersAdapter<Player> adapter = new PlayersAdapter<>(PlayersActivity.this, list);
+            myListView.setAdapter(adapter);
+            Log.e(LOG_TAG, "setting the adapter");
+        }
     }
 }
---
+++ app/src/main/java/com/st00/afir/footballapp/Utils.java
@@ -0,0 +1,151 @@
+package com.st00.afir.footballapp;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zakaria_afir on 06/08/2017.
+ */
+
+public final class Utils {
+
+    /**
+     * Tag for the log messages
+     */
+    private static final String LOG_TAG = Utils.class.getSimpleName();
+
+    /**
+     * fetching players data
+     */
+    public static List<Player> fetchPlayersData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error closing input stream", e);
+        }
+
+        // Extract relevant fields from the JSON response and create an {@link Players} object
+        List<Player> players = extractInformationFromJson(jsonResponse);
+
+        // Return the {@link Players}
+        return players;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the player JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     * Return an {@link Player} object by parsing out information
+     */
+    private static List <Player> extractInformationFromJson(String playerJSON) {
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(playerJSON)) {
+            return null;
+        }
+            List<Player> listOfPlayers = new ArrayList<>();
+            try {
+                JSONArray players = new JSONObject(playerJSON).optJSONArray("players");
+                for (int i = 0; i < players.length(); ++i) {
+                    JSONObject entry = players.optJSONObject(i);
+                    listOfPlayers.add(new Player(entry.getInt("jerseyNumber"),
+                            entry.getString("name"),
+                            entry.getString("nationality"),
+                            entry.getString("dateOfBirth"),
+                            entry.getString("position")));
+                }
+
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Error on JSON: " + e);
+            }
+            return listOfPlayers;
+    }
+}
