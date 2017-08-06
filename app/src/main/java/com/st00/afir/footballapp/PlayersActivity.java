af060644fe239bed3e77a22b99f9a029784a2d56
zakafir <zak.afir@gmail.com>
2017-08-06 17:50:49 +0200

    onPostExecute(...) changes

--- app/src/main/java/com/st00/afir/footballapp/PlayersActivity.java
+++ app/src/main/java/com/st00/afir/footballapp/PlayersActivity.java
@@ -3,9 +3,9 @@ package com.st00.afir.footballapp;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayersActivity extends AppCompatActivity {
@@ -15,19 +15,22 @@ public class PlayersActivity extends AppCompatActivity {
     /**
      * URL for players data
      */
-    private static final String FOOTBALL_REQUEST_URL = "http://api.football-data.org/v1/teams/87/players";
+    private static final String FOOTBALL_REQUEST_URL = "http://api.football-data.org/v1/teams/86/players";
 
     private ListView myListView;
+    private PlayersAdapter<Player> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players);
 
+        myListView = (ListView) findViewById(R.id.list_view_players);
+        adapter = new PlayersAdapter<>(PlayersActivity.this, new ArrayList<Player>());
+        myListView.setAdapter(adapter);
+
         PlayersAsyncTask playersAsyncTask = new PlayersAsyncTask();
         playersAsyncTask.execute(FOOTBALL_REQUEST_URL);
-
-        myListView = (ListView) findViewById(R.id.list_view_players);
 
     }
 
@@ -44,12 +47,14 @@ public class PlayersActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(List<Player> list) {
-            if (list == null) {
-                return;
+            // Clear the adapter of previous data
+            adapter.clear();
+
+            // If there is a valid list of {@link Player}s, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (list != null && !list.isEmpty()) {
+                adapter.addAll(list);
             }
-            PlayersAdapter<Player> adapter = new PlayersAdapter<>(PlayersActivity.this, list);
-            myListView.setAdapter(adapter);
-            Log.e(LOG_TAG, "setting the adapter");
         }
     }
 }
